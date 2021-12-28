31e6af895221383ab0da2b00f7a2056d9de708fb
yasser <yasseralzahed@gmail.com>
2021-12-27 22:46:29 -0500

    initial commit, working camera permissions and preview

---
+++ app/src/main/java/com/example/cameraappviews/Constants.kt
@@ -0,0 +1,12 @@
+package com.example.cameraappviews
+
+import android.Manifest
+
+
+object Constants {
+
+    const val TAG = "cameraX"
+    const val REQUEST_CODE_PERMISSIONS = 123
+    val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+}
